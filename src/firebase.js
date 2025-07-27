e4116856e2fed9910de69f9e3210fe61f240bd9b
Gonzalo Cortez <gcortezff27@gmail.com>
2025-07-26 23:03:23 -0300

    use .env file

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,14 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import firebase from "firebase/compat/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAr4t_39AqZw_M35AFPEE-FXB0cHqxMTLk",
-    authDomain: "quicknotesextension.firebaseapp.com",
-    projectId: "quicknotesextension",
-    storageBucket: "quicknotesextension.firebasestorage.app",
-    messagingSenderId: "153502618542",
-    appId: "1:153502618542:web:09009f539b446e12abee3f"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID
 }
 
 const app = initializeApp(firebaseConfig);
